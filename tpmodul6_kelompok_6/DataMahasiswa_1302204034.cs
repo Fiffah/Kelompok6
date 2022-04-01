eb81a91e361c49d754acd1d41c7b2225c416da6f
jahfal21 <jahfalramadhan4@gmail.com>
2022-04-01 17:31:53 +0700

    menambahkan class DataMahasiswa

---
+++ tpmodul6_kelompok_6/DataMahasiswa_1302204034.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_kelompok_6
+{
+	public class DataMahasiswa_1302204034
+	{
+		public string nama { get; set; }
+		public string nim { get; set; }
+		public string fakultas { get; set; }
+
+        public void ReadJSON()
+		{
+			string fileName = "tp6_1_1302204034.json";
+
+            string jsonString = System.IO.File.ReadAllText(fileName);
+			DataMahasiswa_1302204034 jsonData = JsonSerializer.Deserialize<DataMahasiswa_1302204034>(jsonString);
+
+			this.nama = jsonData.nama;
+			this.nim = jsonData.nim;
+			this.fakultas = jsonData.fakultas;
+
+			Console.WriteLine($"Nama {this.nama} dengan nim {this.nim} dari fakultas {this.fakultas}");
+		}
+	}
+}
--- tpmodul6_kelompok_6/Program.cs
+++ tpmodul6_kelompok_6/Program.cs
@@ -10,6 +10,11 @@ namespace tpmodul6_kelompok_6
     {
         static void Main(string[] args)
         {
+            DataMahasiswa_1302204034 mhs1 = new DataMahasiswa_1302204034();
+            mhs1.ReadJSON();
+
+            KuliahMahasiswa_1302204034 mhs2 = new KuliahMahasiswa_1302204034();
+            mhs2.ReadJson();
         }
     }
 }
